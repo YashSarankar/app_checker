50aedd9e766366e3020ef58566f47b61c6039c0f
Yash S <yashshaft@gmail.com>
2026-02-09 16:58:47 +0530

    Add SnapPlay landing page and subdomain middleware

---
+++ src/middleware.js
@@ -0,0 +1,28 @@
+
+import { NextResponse } from 'next/server'
+
+export const config = {
+    matcher: [
+        /*
+         * Match all paths except for:
+         * 1. /api routes
+         * 2. /_next (Next.js internals)
+         * 3. /_static (inside /public)
+         * 4. all root files inside /public (e.g. /favicon.ico)
+         */
+        "/((?!api/|_next/|_static/|[\\w-]+\\.\\w+).*)",
+    ],
+}
+
+export default async function middleware(req) {
+    const url = req.nextUrl
+    const hostname = req.headers.get("host") || "rank.sarankar.com" // Get hostname (e.g. 'rank.sarankar.com' or 'snapplay.sarankar.com')
+
+    // rewrite root path for snapplay subdomain
+    if (hostname.startsWith("snapplay") && (url.pathname === "/" || url.pathname === "")) {
+        url.pathname = "/snapplay"
+        return NextResponse.rewrite(url)
+    }
+
+    return NextResponse.next()
+}
